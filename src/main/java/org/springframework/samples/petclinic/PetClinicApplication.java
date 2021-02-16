9c39bbf19c031a9c5dcfee94e54008e6176e0f85
Tal Yitzhak <talyi@jfrog.com>
2021-02-16 22:37:34 +0200

    updated distribution part

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -18,7 +18,7 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+  
 /**
  * PetClinic Spring Boot Application.
  *
